e2bd04494c0568c450cd1b347a6f2ab8362b4f32
V-Fries <vincentfries@proton.me>
2025-03-14 16:26:42 +0800

    build: added dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule EtheriumApi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        flags: [
+          :unmatched_returns,
+          :extra_return,
+          :missing_return,
+          :unmatched_returns
+        ]
+      ]
     ]
   end
 
