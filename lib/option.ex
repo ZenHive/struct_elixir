d03ed900d4d8385e5f59cd91c6e1ad2de7cdcedc
V-Fries <vincentfries@proton.me>
2025-03-28 16:07:01 +0800

    feat: Added more utility functions to Option

--- lib/option.ex
+++ lib/option.ex
@@ -16,7 +16,37 @@ defmodule Option do
   """
   @spec map(t(inner), (inner -> new_inner)) :: t(new_inner)
   def map(nil, _f), do: nil
-  def map(value, f), do: f.(value)
+  def map(opt, f), do: f.(opt)
+
+  @doc """
+  Maps an Option.t(inner) to new_inner by applying a function to a contained value
+  (if Some) or returns the default value (if nil).
+
+  If default is expansive to compute, use `map_or_else/3` instead.
+
+  ## Examples
+      iex> Option.map_or("foo", "default", &String.length/1)
+      3
+      iex> Option.map_or(nil, "default", &String.length/1)
+      "default"
+  """
+  @spec map_or(t(inner), new_inner, (inner -> new_inner)) :: new_inner
+  def map_or(nil, default, _f), do: default
+  def map_or(opt, _default, f), do: f.(opt)
+
+  @doc """
+  Maps an Option.t(inner) to new_inner by applying a function to a contained value
+  (if Some) or computes the default value from the given function (if nil).
+
+  ## Examples
+      iex> Option.map_or_else("foo", fn -> "default" end, &String.length/1)
+      3
+      iex> Option.map_or_else(nil, fn -> "default" end, &String.length/1)
+      "default"
+  """
+  @spec map_or_else(t(inner), (-> new_inner), (inner -> new_inner)) :: new_inner
+  def map_or_else(nil, default_f, _f), do: default_f.()
+  def map_or_else(opt, _default_f, f), do: f.(opt)
 
   @doc """
   Returns the contained value if Some, otherwise raises an ArgumentError.
@@ -29,10 +59,12 @@ defmodule Option do
   """
   @spec unwrap!(t(inner)) :: inner
   def unwrap!(nil), do: raise(ArgumentError, "Option.unwrap! called on nil")
-  def unwrap!(value), do: value
+  def unwrap!(opt), do: opt
 
   @doc """
   Returns the contained value if Some, otherwise returns the default value.
+
+  If default is expansive to compute, use `unwrap_or_else/2` instead.
 
   ## Examples
       iex> Option.unwrap_or("foo", "default")
@@ -41,7 +73,7 @@ defmodule Option do
       "default"
   """
   @spec unwrap_or(t(inner), inner) :: inner
-  def unwrap_or(value, default), do: value || default
+  def unwrap_or(opt, default), do: opt || default
 
   @doc """
   Returns the contained value if Some, otherwise computes it from the given function.
