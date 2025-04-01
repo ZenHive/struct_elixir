c6a90e654aaa38d5ab70b0b2cbece3b12562d373
V-Fries <vincentfries@proton.me>
2025-04-01 12:06:18 +0800

    refactor: EthereumApi.Worker now fetches the url from the env itself

--- lib/ethereum_api/application.ex
+++ lib/ethereum_api/application.ex
@@ -2,11 +2,10 @@ defmodule EthereumApi.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      {EthereumApi.Worker, Application.fetch_env!(:ethereum_api, :url)}
-    ]
-
-    opts = [strategy: :one_for_one, name: EthereumApi.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(
+      [EthereumApi.Worker],
+      strategy: :one_for_one,
+      name: EthereumApi.Supervisor
+    )
   end
 end
--- lib/ethereum_api/worker.ex
+++ lib/ethereum_api/worker.ex
@@ -1,13 +1,16 @@
 defmodule EthereumApi.Worker do
-  @spec start_link(url :: String.t() | WebSockex.Conn.t()) :: Result.t(pid(), term())
-  def start_link(url) do
-    JsonRpc.Client.WebSocket.start_link(url, name: __MODULE__)
+  @spec start_link(_ :: any()) :: Result.t(pid(), term())
+  def start_link(_) do
+    JsonRpc.Client.WebSocket.start_link(
+      Application.fetch_env!(:ethereum_api, :url),
+      name: __MODULE__
+    )
   end
 
-  def child_spec(url) do
+  def child_spec(_) do
     %{
       id: __MODULE__,
-      start: {__MODULE__, :start_link, [url]}
+      start: {__MODULE__, :start_link, [Application.fetch_env!(:ethereum_api, :url)]}
     }
   end
 end
