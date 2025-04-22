91e838c41f4469c304f68ef0e42332d38aaa9db3
V-Fries <vincentfries@proton.me>
2025-04-22 14:41:16 +0800

    build: Updated json rpc to 0.5.1

--- mix.exs
+++ mix.exs
@@ -29,9 +29,7 @@ defmodule EthereumApi.MixProject do
     [
       {:ex_doc, "~> 0.37.3", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:result, git: "git@github.com:ZenHive/result_elixir.git", tag: "v0.4.0"},
-      {:option, git: "git@github.com:ZenHive/option_elixir.git", tag: "v0.1.0"},
-      {:json_rpc, git: "git@github.com:ZenHive/json_rpc_elixir.git", tag: "v0.5.0"}
+      {:json_rpc, git: "https://github.com/ZenHive/json_rpc_elixir.git", tag: "v0.5.1"}
     ]
   end
 end
