5982ceaf87755d85557f96b1b2ee4cfdae66b245
V-Fries <vincentfries@proton.me>
2025-04-23 18:21:27 +0800

    release: 0.1.0-b3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EthereumApi.MixProject do
   def project do
     [
       app: :ethereum_api,
-      version: "0.1.0-beta2",
+      version: "0.1.0-b3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
